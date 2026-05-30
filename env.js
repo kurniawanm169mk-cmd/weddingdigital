22053ddf3ab66e6e9b3338fa219f515030ee61eb
kurniawanm169mk-cmd <kurniawanm169.mk@gmail.com>
2026-05-30 19:59:21 +0700

    Add files via upload

--- env.js
+++ env.js
@@ -6,6 +6,6 @@
 
 const ENV = {
     SUPABASE_URL: 'https://nujxevafxamuwspxkhnm.supabase.co',
-    SUPABASE_KEY: 'sb_secret_g3yEmcS8L359W83eeK0gjw_GqBbL6UY',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51anhldmFmeGFtdXdzcHhraG5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxMjg0MTUsImV4cCI6MjA5NTcwNDQxNX0.IAhHpOBw0is4mGmSGJtmMDelP4q1cKAv4UMvRBkURR8',
     ADMIN_PASSWORD: 'digitalweddingfikri'  // GANTI PASSWORD INI SESUAI KEINGINAN ANDA
 };
